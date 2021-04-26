4966ec0d868cab35fbdc73767337f695a2c76abb
leba <leevi.koskinen@hotmail.fi>
2021-04-26 21:50:19 +0300

    Kartta testi!

--- js/data.js
+++ js/data.js
@@ -1,19 +1,33 @@
 'use strict';
 
-
 let latitude = 61.951180799999996;
 let longitude = 28.2066944;
 let distance = 10;
 
 geoFindMe();
 
-const apiurl = `http://lipas.cc.jyu.fi/api/sports-places?closeToLon=${longitude}&closeToLat=${latitude}&closeToMatch=any-point&closeToDistanceKm=${distance}`;
+let apiurl = `http://lipas.cc.jyu.fi/api/sports-places?closeToLon=${longitude}&closeToLat=${latitude}&closeToMatch=any-point&closeToDistanceKm=${distance}`;
+apiurl = `https://citynature.eu/api/wp/v2/places?cityid=5`;
 
 searchIDatAPI(apiurl);
 
 function searchIDatAPI(apiurl)  {
 
-    // suoritetaan hakukysely, fetch hoitaa mahdolliset tietoliikenneongelmat.
+    /*const myRequest = new Request(apiurl, {
+        method: 'GET',
+        dataType: "jsonp",
+        headers: {'Accept': 'application/json'},
+        mode: 'cors',
+        cache: 'default',
+    });
+    console.log(`Sending request to: \n${apiurl}`);
+    fetch(myRequest).then(function(response) {
+        console.log(response);
+        return response.json();
+    }).then(function(json) {
+        showID(json);				// siirrytään varsinaisen datan käsittelyyn.
+    });*/
+
     fetch(apiurl).then(function(response) {
         return response.json();
     }).then(function(json) {
@@ -23,7 +37,7 @@ function searchIDatAPI(apiurl)  {
 
 function showID(json){
     for (let i = 0; i < json.length; i++){
-        console.log(json[i]);
+        console.log(`ID: ${json[i].ID} Title: ${json[i].title}`);
     }
 }
 
