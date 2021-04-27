a22ab969924b5e82fd0ff4056719605772b4ff1b
PeetuSa <peetu.salonen@gmail.com>
2021-04-27 20:43:17 +0300

    Navigation muuttaminen inner HTML:stä DOM:iin

--- js/navigation.js
+++ js/navigation.js
@@ -1,38 +1,33 @@
-'use strict'
+'use strict';
 
-const nav = document.querySelector("nav");
-const body = document.querySelector("body");
+const body = document.querySelector('body');
+const nav = document.createElement('nav');
+const ul = document.createElement('ul');
 
 let filename = location.href.split('/').pop();
 filename = filename.split('?')[0];
 console.log(`navigation.js: ${filename}`);
 
+let siteTopic = ['Etusivu', 'Kartta', 'Tietoa'];
 let files = ["etusivu.html", "kartta.html", "tietoa.html"];
-let activefile = ["", "", "", ""];
-for(let i = 0; i < files.length; i++){
-    if(filename === files[i]){
-        activefile[i] = "active";
+
+body.appendChild(nav);
+nav.appendChild(ul);
+ul.classList.add('navul');
+
+for(let j = 0; j < files.length; j++) {
+
+    const li = document.createElement('li');
+    const article = document.createElement('a');
+
+    ul.appendChild(li);
+    li.classList.add('navli');
+    li.appendChild(article);
+    article.classList.add('nava');
+    article.href = files[j];
+    article.textContent = siteTopic[j];
+
+    if(filename === files[j]){
+        article.classList.add("active");
     }
-}
-const html =
-    `
-    <nav>
-        <ul class="navul">
-                <li class="navli">
-                    <a class="${activefile[0]} nava" href=${files[0]}>Etusivu</a>
-                </li>
-                <li class="navli">
-                    <a class="${activefile[1]} nava" href=${files[1]}>Kartta</a>
-                </li>
-                <li class="navli">
-                    <a class="${activefile[2]} nava"href=${files[2]}>Tietoa</a>
-                </li>
-        </ul>
-    </nav>
-    `;
-if(nav != null){
-    nav.innerHTML = html;
-} 
-else{
-    body.innerHTML = (html + body.innerHTML);
 }
